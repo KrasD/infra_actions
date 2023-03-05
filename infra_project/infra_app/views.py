76bbf6258cf316a69f98545ada6eb99f23785f52
Дмитрий Красильников <Kras.Dmitriy95@yandex.ru>
2023-03-05 22:40:02 +0300

    update5

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,11 +1,9 @@
-import requests
-
 from django.http import HttpResponse
 
 
-def index(requests):
+def index(request):
     return HttpResponse('У меня получилось!')
 
 
-def second_page(requests):
-    return HttpResponse('А это вторая страница')
+def second_page(request):
+    return HttpResponse('А это вторая страница!')
